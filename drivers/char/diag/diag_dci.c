4cc6795be3ead03033d8b7cad25de1884a0bb21f
hehao <hehao@xiaomi.com>
2018-05-18 14:35:16 +0800

    Kernel: Xiaomi kernel changes for Redmi Note5, Redmi Note6 Pro and XiaoMi 6X
    The Patch based on QualComm release TAG:LA.UM.6.2.r1-06100-sdm660.0
    The kernel config file used are whyred-perf_defconfig, tulip-perf_defconfig and wayne-perf_defconfig
    
    Change-Id: Ic50efc4c0209a5bab3d564e7576118035aefdc26
    Signed-off-by: hehao <hehao@xiaomi.com>

--- drivers/char/diag/diag_dci.c
+++ drivers/char/diag/diag_dci.c
@@ -1381,6 +1381,7 @@ void extract_dci_log(unsigned char *buf, int len, int data_source, int token,
 		pr_err("diag: In %s buffer is NULL\n", __func__);
 		return;
 	}
+
 	/*
 	 * The first eight bytes for the incoming log packet contains
 	 * Command code (2), the length of the packet (2), the length
@@ -1425,6 +1426,10 @@ void extract_dci_ext_pkt(unsigned char *buf, int len, int data_source,
 		return;
 	}
 
+	if (len < (EXT_HDR_LEN + sizeof(uint8_t))) {
+		pr_err("diag: In %s invalid len: %d\n", __func__, len);
+		return;
+	}
 	version = *(uint8_t *)buf + 1;
 	if (version < EXT_HDR_VERSION)  {
 		pr_err("diag: %s, Extended header with invalid version: %d\n",
